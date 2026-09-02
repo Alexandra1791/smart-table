eb0ac799b77bb36d1e10e68aff7300ffaaaa91b1
Alexandra Ch <alexandra170191@yandex.ru>
2026-09-02 18:17:56 +0300

    release 02.09

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -4,11 +4,38 @@ import {createComparison, defaultRules} from "../lib/compare.js";
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
+    Object.keys(indexes).forEach((elementName)=> {
+        if (elements[elementName]) {
+            elements[elementName].append(
+                ...Object.values(indexes[elementName]).map(name => {
+                    const option = document.createElement('option');
+                    option.value = name;
+                    option.textContent = name;
+                    return option;
+                })
+            );
+        }
+    });
+
+    const compare = createComparison(defaultRules);
 
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
+        if (action && action.name === 'clear') {
+            const parent = action.parentElement;
+            const input = parent.querySelector('input');
+
+            if(input) {
+                input.value = '';
+            }
+            const targetField = action.dataset.field;
+            if (targetField && targetField in state) {
+                state[targetField] = '';
+            }
+        }
+        
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
